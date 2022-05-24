c4e0671edd2679d7cd34f75e4fe9ed0921cce696
HoracioGutierrez <horacio.estevez@gmail.com>
2022-05-24 18:18:30 -0300

    MongoDB

--- clase_05/app/server/server.js
+++ clase_05/app/server/server.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const path = require('path')
 const bodyparser = require('body-parser')
+//DRIVER DE MONGODB (mongodb - MONGOOSE (ORM) ) 
+const mongoose = require("mongoose")
+
+//schema : Las validaciones de los datos
+//modelo : Es la representacion de una coleccion 
+//Docuemto/Instancias de cada modelo : Es la representacion de un documento existente o no (new Model)
+
+
+
+
+const UsuariosSchema = new mongoose.Schema({ 
+  nombre :  {
+    type : String,
+    required : true
+  },
+  edad : {
+    type : Number,
+    required : true
+  }
+})
+
+const Usuarios = mongoose.model("Usuarios", UsuariosSchema)
+
+
+
+const documento = new Usuarios({nombre:"Juan",edad:45})
+
+documento.save()
+
+
+
 
 const app = express()
 
@@ -11,7 +42,7 @@ let personajes = [
     bio: "El mejor personaje de todos los tiempos",
     img: "https://i.pinimg.com/originals/f4/b6/b1/f4b6b1c8f9f8f8c8f8f8f8f8f8f8f8f8.jpg"
   }
-];
+]
 
 //console.log(`${__dirname}/index.html`)
 //console.log(path.resolve(__dirname, 'index.html'))
@@ -59,4 +90,13 @@ app.get('/personajes',(req,res)=>{
   res.send("API")
 }) */
 
-app.listen(3001,console.log('app escuchando en el puerto 3001'))
+
+const conexion = mongoose.connect("mongodb://localhost:27017/personajes")
+
+.then((res)=>{
+  console.log("Conexion exitosa a la base de datos")
+  app.listen(3001,console.log('app escuchando en el puerto 3001'))
+})
+.catch((err)=>{
+  console.log(err)
+})
